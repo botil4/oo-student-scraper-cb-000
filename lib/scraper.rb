abac48e9f68c1482b8b2f6c7d4bd65bcefa5f77c
Travis Clarkson <tk1667@gmail.com>
2018-03-04 20:35:26 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,9 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+    name = doc.css(".student-card").text
   end
 
   def self.scrape_profile_page(profile_url)
